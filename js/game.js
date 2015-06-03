66202be51ea50be3701c6c32392fbb1167597a83
LHSP <lhsperez@gmail.com>
2015-06-03 00:00:44 -0300

    First mechanics added

--- js/game.js
+++ js/game.js
@@ -1,55 +1,139 @@
 var game = {
 	resources: {
 		dust: 0,
-		brick: 0
+		brick: 0,
+		fire: 0,
+
+		change: function(name, quantity){
+			if(game.resources[name] != null && (quantity >= 0 || (quantity < 0 && game.resources[name] >= (-quantity)))){
+				game.resources[name] += quantity;
+				return true;
+			}
+			return false;
+		}
 	},
 	parameters: {
 		dustPerClick: 1,
-		dustToBrick: 10,
-		bricksPerDust: 1
+		dustToBrick: -10,
+		bricksPerDust: 1,
+		fireBurningPerTick: -1,
+		dustToFire: -5,
+		firePerKindle: 50,
+		strongFireCount: 1500
 	},
+	flags: {
+		canMakeBricks: false,
+		thunderStruck: false,
+		hasFire: false,
+		firstKindle: true,
+		strongFire: false,
+		hasBricks: false
+	},
+	strings: {
+		gatherDustButton: 'Gather Dust',
+		meltDustButton: 'Melt dust together',
+		createBrickButton: 'Create Brick',
+		kindleFire: 'Kindle Fire'
+	},
+	messages: {
+		cantCreateBricks: 'There is not enough dust to create a brick!',
+		cantCreateAllBricks: function(numberOfBricksCreated, numberOfBricksAsked) {
+			return 'There is not enough dust to create {0} bricks. Created {1} bricks.'.format(numberOfBricksAsked, numberOfBricksCreated);
+		},
+		thunderStruck: 'A thunder has hit the dust near you. The dust is on fire!',
+		noDustToKindle: 'Not enough dust to kindle the fire with',
+		firstKindle: 'You start to feel \'not cold\' for the first time in a long while.',
+		strongFire: 'The fire seems to be really strong now. Maybe we can cook something'
+	},
+	events: [
+		{
+			name: 'Thunder',
+			chance: 0.5, 
+			happens: function(){ 
+				if(!game.flags.hasFire){
+					game.resources.fire += 1000;
+					game.flags.thunderStruck = true;
+					game.flags.hasFire = true;
+					game.showMessage(game.messages.thunderStruck);
+				}
+			}
+		}
+	],
+
+
+	// Resource functions
 	gatherDust: function(){
-		game.resources.dust += game.parameters.dustPerClick;
+		game.resources.change('dust', game.parameters.dustPerClick);
+	},
+	kindleFire: function(){
+		if(game.resources.change('dust', game.parameters.dustToFire)){
+			game.resources.change('fire', game.parameters.firePerKindle);
+			if(game.flags.firstKindle){
+				game.flags.firstKindle = false;
+				game.showMessage(game.messages.firstKindle);
+			}
+		}
+		else{
+			game.showMessage(game.messages.noDustToKindle);
+		}
+	},
+	createBrick: function(numberOfBricks){
+		for(i = 0; i < numberOfBricks; i++){
+			if(game.resources.dust >= game.parameters.dustToBrick){
+				game.resources.change('dust', game.parameters.dustToBrick);
+				game.resources.change('brick', game.parameters.bricksPerDust);
+			} else {
+				if(i == 0){
+					game.showMessage(game.messages.cantCreateBricks);
+				} else {
+					game.showMessage(game.messages.cantCreateAllBricks(i, numberOfBricks));
+				}
+				break;
+			}
+		}
+	},
+	updateResources: function(){
+		game.resources.change('fire', game.parameters.fireBurningPerTick);
+		if(game.resources.fire >= game.parameters.strongFireCount && !game.flags.strongFire){
+			game.flags.strongFire = true;
+			if(game.resources.brick <= 0){
+				game.showMessage(game.messages.strongFire);
+			}
+		}
+		if(game.resources.fire < game.parameters.strongFireCount && game.flags.strongFire){
+			game.flags.strongFire = false;
+		}
+	},
+	showResources: function(){
+		if(game.flags.hasFire){
+			$('.kindleFire').removeClass('hidden');
+			$('.kindleFire').html(game.strings.kindleFire);
+		}
+		if(game.flags.strongFire){
+			game.flags.hasBricks = true;
+			$('.createBrick').attr("disabled", false);
+		}
+		else{
+			$('.createBrick').attr("disabled", true);
+		}
+		if(game.flags.hasBricks){
+			$('.createBrick').removeClass('hidden');
+			$('.createBrick').html(game.strings.meltDustButton);
+			$('.brickCount').removeClass('hidden');
+		}
 	},
 	tick: function(){
+		game.updateResources();
 		//console.log('ticked');
 		$('.dustCount').html(game.resources.dust);
 		$('.brickCount').html(game.resources.brick);
+		$('.fireCount').html(game.resources.fire);
 		game.showResources();
 	},
 	autoSave: function(){
 		$('.autosaveIndicator').addClass('fadeIn');
 		game.save();
 		setTimeout(function() { $('.autosaveIndicator').removeClass('fadeIn'); }, 2000);
-	},
-	showResources: function(){
-		if(game.resources.brick > 0 || game.resources.dust >= game.parameters.dustToBrick){
-			$('.brickCount').removeClass('hidden');
-			$('.createBrick').removeClass('hidden');
-		}
-	},
-	resTransforms: {
-		createBrick: function(numberOfBricks){
-			for(i = 0; i < numberOfBricks; i++){
-				if(game.resources.dust >= game.parameters.dustToBrick){
-					game.resources.dust -= game.parameters.dustToBrick;
-					game.resources.brick += game.parameters.bricksPerDust;
-				} else {
-					if(i == 0){
-						game.showMessage(game.messages.cantCreateBricks);
-					} else {
-						game.showMessage(game.messages.cantCreateAllBricks(i, numberOfBricks));
-					}
-					break;
-				}
-			}
-		}
-	},
-	messages: {
-		cantCreateBricks: 'There is not enough dust to create a brick!',
-		cantCreateAllBricks: function(numberOfBricksCreated, numberOfBricksAsked) {
-			return 'There is not enough dust to create {0} bricks. Created {1} bricks.'.format(numberOfBricksAsked, numberOfBricksCreated);
-		}
 	},
 	showMessage: function(message){
 		var messageString = '<div>' + message + '</div>'
@@ -61,30 +145,58 @@ var game = {
 
 
 
-
-
+	doEvents: function() {
+		for (var i = game.events.length - 1; i >= 0; i--) {
+			var thisChance = Math.random();
+			if(thisChance <= game.events[i].chance)
+				game.events[i].happens();
+		};
+	},
 	init: function(){
+		game.originalResources = game.resources;
 		game.load();
+	},
+	reset: function(){
+		game.resources = game.originalResources;
+		game.save();
+		location.reload();
 	},
 	save: function(){
 		localStorage.setItem("resources",JSON.stringify(game.resources));
 		localStorage.setItem("parameters",JSON.stringify(game.parameters));
+		localStorage.setItem("flags",JSON.stringify(game.flags));
 	},
 	load: function(){
-		game.resources = JSON.parse(localStorage.getItem("resources"));
-		game.parameters = JSON.parse(localStorage.getItem("parameters"));
+		var resources = JSON.parse(localStorage.getItem("resources"));
+		$.extend(game.resources, resources);
+		var parameters = JSON.parse(localStorage.getItem("parameters"));
+		$.extend(game.parameters, parameters);
+		var flags = JSON.parse(localStorage.getItem("flags"));
+		$.extend(game.flags, flags);
 	}
 };
 
 // Bindings
 $(document).ready(function(){
 	game.init();
+	$('.progress-button').progressInitialize();
+	$('.gatherDust').html(game.strings.gatherDustButton);
+	$('.createBrick').html(game.strings.createBrickButton);
 });
 $(document).on('click', '.gatherDust', function(){
 	game.gatherDust();
 });
 $(document).on('click', '.createBrick', function(){
-	game.resTransforms.createBrick(1);
+	if(!game.flags.canMakeBricks && game.flags.thunderStruck){
+		$('.brickCount').removeClass('hidden');
+		$('.createBrick').html(game.strings.createBrickButton);
+		game.flags.canMakeBricks = true;
+	}
+
+	game.createBrick(1);
+});
+$(document).on('click', '.kindleFire', function(){
+	game.kindleFire();
 });
 $(document).on('click', '.saveGame', function(){
 	game.save();
@@ -92,11 +204,17 @@ $(document).on('click', '.saveGame', function(){
 $(document).on('click', '.loadGame', function(){
 	game.load();
 });
+$(document).on('click', '.resetGame', function(){
+	if(confirm('Are you sure you want to reset the game?')){
+		game.reset();
+	};
+});
 var timerIterations = 0;
 var timer = setInterval(function () { 
 							game.tick();
 							if(timerIterations > 50) {
 								timerIterations = 0;
+								game.doEvents();
 								game.autoSave();
 							} else {
 								timerIterations++;
